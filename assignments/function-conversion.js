a2b3f0f9a25f237e38113a3040e6f12ab16e46c8
David Kuseh <davegru203@gmail.com>
2019-08-20 08:14:12 +0000

    made corrections in array.js, and completed exercises in function-conversion.js

--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -4,22 +4,31 @@
 // console.log("Function was invoked!");
 // };
 // myFunction();
+var myFunction = (x, y) => console.log(x*y);
+myFunction(2,2);
 
 // let anotherFunction = function (param) {
 //   return param;
 // };
 // anotherFunction("Example");
+var anotherFunction = param => console.log(param);
+anotherFunction('anotherFunction');
 
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
+var add = (param1, param2) => console.log(param1 + param2);
+add(1,2);
+
 
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
 
+var subtract = (param1, param2) => console.log(param1 - param2);
+subtract(1,2);
 
 // Stretch
 
@@ -28,3 +37,9 @@
 //   return num * 3;
 // });
 // console.log(triple);
+exampleArray = [1,2,3,4];
+const triple = exampleArray.map(function(num) {
+    return num*3;
+});
+
+console.log(triple);
